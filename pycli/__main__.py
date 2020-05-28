4fb5c4276ebe2b8aaf0f016137788f614de4fed8
pjammu <31864448+pjammu@users.noreply.github.com>
2020-05-28 19:52:06 -0400

    Create __main__.py

---
+++ pycli/__main__.py
@@ -0,0 +1,26 @@
+import sys, argparse
+#from pycli.Functions.print_argument import print_arg
+#from Classes.classmodule import MyClass
+
+my_parser=argparse.ArgumentParser()
+#my_parser.add_argument('--num1', required=True, type=int)
+#my_parser.add_argument('--num2', required=True, type=int)
+my_parser.add_argument('--name', required=True, type=str)
+
+args=my_parser.parse_args()
+
+def main():
+
+	#my_object = MyClass(args.name)
+	#prod=my_object.multiply()
+	#total=my_object.sum()
+
+	#phrase=my_object.print_argument()
+  phrase='Hello '+str(args.name)
+	print (phrase)
+	#print (total)
+
+
+if __name__ == '__main__':
+	main()
+
